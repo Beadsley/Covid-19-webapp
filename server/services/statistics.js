776696441c810ace483af0fef7033a8835ea0739
Beadsley <danbeadleson@gmail.com>
2020-05-10 08:42:51 +0200

    Response returns array

--- server/services/statistics.js
+++ server/services/statistics.js
@@ -1,19 +1,23 @@
 const config = require('../config/config');
 
 const evalData = (data) => {
-  let statesStats = {};
+  let statesStats = [];
   const totalStates = 50;
   const last3daysData = data.slice(0, totalStates * 3);
-  last3daysData.forEach((dailydata) => {
-    if (statesStats[dailydata.state] === undefined) {
-      statesStats[dailydata.state] = {
+
+  last3daysData.forEach((dailydata, index) => {
+    if (index < 50) {
+      statesStats.push({
+        state: dailydata.state,
         deaths: dailydata.death,
+        last3daysDeaths: dailydata.death,
         hospitalizedCurrently: dailydata.hospitalizedCurrently,
         name: config.ENUMS.STATES[dailydata.state],
-      };
-    } else if (statesStats[dailydata.state] !== undefined) {
-      statesStats[dailydata.state].deaths =
-        dailydata.death + statesStats[dailydata.state].deaths;
+        recovered: dailydata.recovered,
+      });
+    } else {
+      const index = statesStats.findIndex((data) => data.state === dailydata.state);
+      index !== -1 && (statesStats[index].last3daysDeaths = statesStats[index].last3daysdeaths + dailydata.death);
     }
   });
   return statesStats;
